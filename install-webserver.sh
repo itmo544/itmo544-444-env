0098f191db3f650eb4688bf6f479672b4ac5181f
Sultan <deepclean911@yahoo.com>
2015-10-02 17:15:55 -0500

    Added code to pull new index.html from other github account

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
+sudo apt-get install -y apache2 git 
 
-sudo apt-get install -y apache2
+git clone https://github.com/itmo544/itmo544-444-fall2015.git
+
+mv ./itmo544-444-fall2015/images /var/www/html/images
+mv ./itmo544-444-fall2015/index.html /var/www/html
 
 echo "Hello! Sultan Here!" > /tmp/hello.txt
