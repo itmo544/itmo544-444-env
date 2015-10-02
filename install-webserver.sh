053f8e37ba7494df41c42136be835fd73bfb9376
Sultan <deepclean911@yahoo.com>
2015-10-02 18:55:35 -0500

    Added code to pull new index.html and page2 from other github account

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y apache2 git
 
 git clone https://github.com/itmo544/itmo544-444-fall2015.git
 
-mv ./itmo544-444-fall2015/images /var/www/html/images
-mv ./itmo544-444-fall2015/index.html /var/www/html
+cp ./itmo544-444-fall2015/images /var/www/html/images
+cp ./itmo544-444-fall2015/index.html /var/www/html
+cp ./itmo544-444-fall2015/page2.html /var/www/html
 
 echo "Hello! Sultan Here!" > /tmp/hello.txt
