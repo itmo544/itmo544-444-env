c4bf8e608b88ba7f1c38d411005f8be6a2ee8431
Sultan <deepclean911@yahoo.com>
2015-10-02 20:14:48 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://itmo544-444-env/install-webserver.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://itmo544-444-env/install-env.sh --debug
