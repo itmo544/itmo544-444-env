64cb381e1214c64e1b598562adf9ad103278c168
Sultan <deepclean911@yahoo.com>
2015-10-23 19:44:09 -0500

    Update - Merged install-webserver code install-env code.

--- install-env.sh
+++ install-env.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git 
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/itmo544/itmo544-444-fall2015.git
 
-cp ./itmo544-444-fall2015/images /var/www/html/images
-cp ./itmo544-444-fall2015/index.html /var/www/html
-cp ./itmo544-444-fall2015/page2.html /var/www/html
+mv ./itmo544-444-fall2015/images /var/www/html/images
+mv ./itmo544-444-fall2015/index.html /var/www/html
+mv ./itmo544-444-fall2015/page2.html /var/www/html
+mv ./itmo544-444-fall2015/*.php /var/www/html
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
 
 echo "Hello! Sultan Here!" > /tmp/hello.txt
