1ba9243a17b3ffe8180181fe52a78a0a0163b3ba
Sultan <deepclean911@yahoo.com>
2015-11-06 17:51:04 -0600

    fixed evaluation period, commend out rds

--- launch.sh
+++ launch.sh
@@ -33,9 +33,9 @@ for i in {0..100}; do echo -ne '.'; sleep 1; done
 
 
 # Create Launch Configuration and Auto Scale
-aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id ami-d05e75b8 --instance-type t2.micro --key-name itmo-linux-troubleshootingkey --security-groups sg-e30e4b84 --user-data file://install-env.sh --iam-instance-profile phpdeveloperRole
+#aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id ami-d05e75b8 --instance-type t2.micro --key-name itmo-linux-troubleshootingkey --security-groups sg-e30e4b84 --user-data file://install-env.sh --iam-instance-profile phpdeveloperRole
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-extended-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-name itmo544sb-lb --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-c856a5f5
+#aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-extended-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-name itmo544sb-lb --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-c856a5f5
 
 
 #Scaling policy for cloud watch
@@ -51,7 +51,7 @@ aws cloudwatch put-metric-alarm --alarm-name cpumon10 --alarm-description "Alarm
 
 
 #Create Database
-aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
+#aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
 
 #Wait Untill Database is created and then print
 aws rds wait db-instance-available --db-instance-identifier mp1-sb
