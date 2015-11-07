fec59d875485b9be09d3859a6bc54a92a6491199
Sultan <deepclean911@yahoo.com>
2015-11-06 18:47:27 -0600

    comment out auto scale, cloud watch, trying to connect to database through launhc. shs and create table if not created by setup.php

--- launch.sh
+++ launch.sh
@@ -45,9 +45,9 @@ SCALEDOWN=(`aws autoscaling put-scaling-policy --auto-scaling-group-name itmo-54
 
 
 #Cloud Watch Using Auto Scale Policy
-aws cloudwatch put-metric-alarm --alarm-name cpumon30 --alarm-description "Alarm when CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=AutoScalingGroupName,Value=itmo-544-extended-auto-scaling-group-2" --evaluation-periods 1 --alarm-actions $SCALEUP --unit Percent
+#aws cloudwatch put-metric-alarm --alarm-name cpumon30 --alarm-description "Alarm when CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=AutoScalingGroupName,Value=itmo-544-extended-auto-scaling-group-2" --evaluation-periods 1 --alarm-actions $SCALEUP --unit Percent
 
-aws cloudwatch put-metric-alarm --alarm-name cpumon10 --alarm-description "Alarm when CPU drops below 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 10 --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=AutoScalingGroupName,Value=itmo-544-extended-auto-scaling-group-2" --evaluation-periods 1 --alarm-actions $SCALEDOWN --unit Percent
+#aws cloudwatch put-metric-alarm --alarm-name cpumon10 --alarm-description "Alarm when CPU drops below 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 10 --comparison-operator LessThanOrEqualToThreshold  --dimensions "Name=AutoScalingGroupName,Value=itmo-544-extended-auto-scaling-group-2" --evaluation-periods 1 --alarm-actions $SCALEDOWN --unit Percent
 
 
 #Create Database
@@ -56,9 +56,23 @@ aws cloudwatch put-metric-alarm --alarm-name cpumon10 --alarm-description "Alarm
 #Wait Untill Database is created and then print
 aws rds wait db-instance-available --db-instance-identifier mp1-sb
 
+#Create table if not created by setup.php
+	# Connect to database instance
+		# Connect to database
+			# Create table if doesn't exists
+				# Show Schema 
 
+mysql -h mp1-sb.cq1yqny3b3jn.us-east-1.rds.amazonaws.com -P 3306 -u controller -pletmein888  << EOF
 
+use customerrecords;
 
+CREATE TABLE IF NOT EXISTS items(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(200) NOT NULL, email VARCHAR(200) NOT NULL, phone VARCHAR(20) NOT NULL, s3rawurl VARCHAR(255) NOT NULL, s3finishedurl VARCHAR(255) NOT NULL, jpgfilename VARCHAR(255) NOT NULL, status INT NOT NULL, tdate DATETIME DEFAULT CURRENT_TIMESTAMP);
 
+show tables;
+
+EOF
+
+#Launch Load balancer in Web Browser
+firefox $ELBURL
 
 
