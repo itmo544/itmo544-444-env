d9dfa3ab905b243e1f3bcabc13ffd1125ee2d52c
Sultan <deepclean911@yahoo.com>
2015-11-17 00:43:18 -0600

    YAYgit add launch-orig.sh EVERYTHING WORKDS

--- launch-orig.sh
+++ launch-orig.sh
@@ -27,9 +27,8 @@ aws elb configure-health-check --load-balancer-name itmo544sb-lb --health-check 
 aws elb create-lb-cookie-stickiness-policy --load-balancer-name itmo544sb-lb --policy-name cookie-policy --cookie-expiration-period 90
 aws elb set-load-balancer-policies-of-listener --load-balancer-name itmo544sb-lb --load-balancer-port 80 --policy-names cookie-policy
 
-echo -e "\wait 3 minutes for ELB before it starts loading in browser"
-for i in {0..100}; do echo -ne '.'; sleep 1; done
-
+echo -e "\wait 8-10 minutes for ELB before it starts loading in browser"
+for i in {0..540}; do echo -ne ':-)'; sleep 1; done
 
 # Create Launch Configuration and Auto Scale
 #aws autoscaling create-launch-configuration --launch-configuration-name itmo544-launch-config --image-id ami-d05e75b8 --instance-type t2.micro --key-name itmo-linux-troubleshootingkey --security-groups sg-e30e4b84 --user-data file://install-env.sh --iam-instance-profile phpdeveloperRole
@@ -50,32 +49,39 @@ for i in {0..100}; do echo -ne '.'; sleep 1; done
 
 
 #Create Database
-aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --engine-ver 5.6.23 --master-username controller --master-user-password letmein888 --allocated-storage 10 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
+#aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --engine-ver 5.6.23 --master-username controller --master-user-password letmein888 --allocated-storage 10 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
 
 #Wait Untill Database is created
-aws rds wait db-instance-available --db-instance-identifier mp1-sb
+#aws rds wait db-instance-available --db-instance-identifier mp1-sb
 
 #Create Read Replica Golden Copy
-aws rds create-db-instance-read-replica --db-instance-identifier mp1-sb-rr --source-db-instance-identifier mp1-sb --publicly-accessible
+#aws rds create-db-instance-read-replica --db-instance-identifier mp1-sb-rr --source-db-instance-identifier mp1-sb --publicly-accessible
 
 #Create an EndPoint
-DBEndpoint=('aws rds describe-db-instances --output text | grep ENDPOINT | sed -e "s/3306//g" -e "s/ //g" -e "s/ENDPOINT//g"');
+DBEndpoint=(`aws rds describe-db-instances --output text | grep ENDPOINT | sed -e "s/3306//g" -e "s/ //g" -e "s/ENDPOINT//g"`);
+echo ${DBEndpoint[@]}
+
+#chmod 755 ../itmo544-444-fall2015/setup.php
+#sudo php ../itmo544-444-fall2015/setup.php
 
 #Create table if not created by setup.php
 	# Connect to database instance
 		# Connect to database
-			# Create table if doesn't exists
+			# Create table (Forget setup.php)
 				# Show Schema 
 
-#mysql -h mp1-sb.cq1yqny3b3jn.us-east-1.rds.amazonaws.com -P 3306 -u controller -pletmein888  << EOF
+mysql -h ${DBEndpoint[@]} -P 3306 -u controller -pletmein888  << EOF
 
-#use customerrecords;
+use customerrecords;
 
-#CREATE TABLE IF NOT EXISTS items(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, uname VARCHAR2(20) NOT NULL, email VARCHAR2(20) NOT NULL, phone VARCHAR2(20) NOT NULL, s3rawurl VARCHAR2(255) NOT NULL, s3finishedurl VARCHAR2(255) NOT NULL, jpgfilename VARCHAR2(255) NOT NULL, status TINYINT(3)CHECK(state IN(0,1,2)), tdate DATETIME DEFAULT CURRENT_TIMESTAMP);
-#show tables;
-#EOF
+CREATE TABLE items (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(20) NOT NULL, email VARCHAR(20) NOT NULL, phone VARCHAR(20) NOT NULL, s3rawurl VARCHAR(255) NOT NULL, s3finishedurl VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, status TINYINT(3)CHECK(state IN(0,1,2)), date DATETIME DEFAULT CURRENT_TIMESTAMP);
+
+show tables;
+
+EOF
+
+
+echo "YAY!! It worked!!";
 
 #Launch Load balancer in Web Browser
 firefox $ELBURL
-
-
