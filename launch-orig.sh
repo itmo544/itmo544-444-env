6c9b5bd08ade71ebc6fc32c1262552a5368a90ff
Sultan <deepclean911@yahoo.com>
2015-11-27 17:52:12 -0600

    test enviornment

--- launch-orig.sh
+++ launch-orig.sh
@@ -11,7 +11,12 @@
 
 #################################
 
-#./cleanup.sh
+echo "===============================================================";
+echo "Cleaning up previous Instances, Load Blacer, Autoscale, and RDS";
+echo "===============================================================";
+
+#./cleanup-def.sh
+./cleanup.sh
 
 # declare an array in bash
 declare -a instanceARRAY
@@ -43,8 +48,8 @@ aws elb configure-health-check --load-balancer-name itmo544sb-lb --health-check 
 #aws elb create-lb-cookie-stickiness-policy --load-balancer-name itmo544sb-lb --policy-name cookie-policy --cookie-expiration-period 90
 #aws elb set-load-balancer-policies-of-listener --load-balancer-name itmo544sb-lb --load-balancer-port 80 --policy-names cookie-policy
 
-echo -e "Wait 25 seconds for ELB"
-for i in {0..25}; do echo -ne '.'; sleep 1; done
+#echo -e "Wait 25 seconds for ELB"
+#for i in {0..25}; do echo -ne '.'; sleep 1; done
 
 echo "................";
 echo "Creating Autoscale and Cloudwatch Metrics";
@@ -73,10 +78,10 @@ echo "Creating database, wait 10-15 minutes";
 echo "=====================================";
 
 #Create Database
-aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --engine-ver 5.6.23 --master-username controller --master-user-password letmein888 --allocated-storage 10 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
+#aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-sb --db-instance-class db.t1.micro --engine MySQL --engine-ver 5.6.23 --master-username controller --master-user-password letmein888 --allocated-storage 10 --vpc-security-group-ids sg-e30e4b84 --publicly-accessible
 
 #Wait Untill Database is created
-aws rds wait db-instance-available --db-instance-identifier mp1-sb
+#aws rds wait db-instance-available --db-instance-identifier mp1-sb
 
 #Create Read Replica Golden Copy
 #aws rds create-db-instance-read-replica --db-instance-identifier mp1-sb-rr --source-db-instance-identifier mp1-sb --publicly-accessible
@@ -85,8 +90,8 @@ aws rds wait db-instance-available --db-instance-identifier mp1-sb
 #sudo php ../itmo544-444-fall2015/setup.php
 
 #Create an EndPoint
-DBEndpoint=(`aws rds describe-db-instances --output text | grep ENDPOINT | sed -e "s/3306//g" -e "s/ //g" -e "s/ENDPOINT//g"`);
-echo ${DBEndpoint[0]}
+#DBEndpoint=(`aws rds describe-db-instances --output text | grep ENDPOINT | sed -e "s/3306//g" -e "s/ //g" -e "s/ENDPOINT//g"`);
+#echo ${DBEndpoint[0]}
 
 #Create table if not created by setup.php
 	# Connect to database instance
@@ -98,7 +103,7 @@ echo ${DBEndpoint[0]}
 
 #use customerrecords;
 
-#CREATE TABLE IF NOT EXISTS items (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(20) NOT NULL, email VARCHAR(20) NOT NULL, phone VARCHAR(20) NOT NULL, s3rawurl VARCHAR(255) NOT NULL, s3finishedurl VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, status TINYINT(3)CHECK(state IN(0,1,2)), date DATETIME DEFAULT CURRENT_TIMESTAMP);
+#CREATE TABLE IF NOT EXISTS items (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(20) NOT NULL, email VARCHAR(30) NOT NULL, phone VARCHAR(20) NOT NULL, s3rawurl VARCHAR(255) NOT NULL, s3finishedurl VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, status TINYINT(3)CHECK(state IN(0,1,2)), date DATETIME DEFAULT CURRENT_TIMESTAMP);
 
 #show tables;
 
@@ -136,9 +141,10 @@ echo "==============================================";
 
 # Everything is working
 
-echo -e "Wait 90 seconds for ELB"
-for i in {0..90}; do echo -ne '.'; sleep 1; done
+echo -e "Wait 2.5 minutes for ELB"
+for i in {0..150}; do echo -ne '.'; sleep 1; done
 
+echo "";
 echo "=================================================================";
 echo "Everything is successfully created. Now launching ELB in Firefox";
 echo "=================================================================";
