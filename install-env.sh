5a24ed2ba3466c6a5e02029bf7430f764c47455b
Sultan <deepclean911@yahoo.com>
2015-11-29 20:25:04 -0600

    added imagic

--- install-env.sh
+++ install-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick
 
 git clone https://github.com/itmo544/itmo544-444-fall2015.git
 
