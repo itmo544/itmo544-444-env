58ae787269bfa40339ed7c81a71245be2851d98e
Sultan <deepclean911@yahoo.com>
2015-12-02 02:03:34 -0600

    changed protocol from sms to email, sms works fine

--- launch.sh
+++ launch.sh
@@ -131,13 +131,13 @@ echo "This is the ARN: $ARN"
 aws sns set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --attribute-value mp2
 
 #SUBSCRIBE
-aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 16303621844
+aws sns subscribe --topic-arn $ARN --protocol email --notification-endpoint sbaig4@hawk.iit.edu
 
 echo -e "Wait 30 seconds for Pending Confirmation"
 for i in {0..30}; do echo -ne ':)'; sleep 1; done
 
 #PUBLISH
-aws sns publish --topic-arn "arn:aws:sns:us-east-1:882985546393:mp2" --message "Congratulations, you sucessfully subscribed"
+#aws sns publish --topic-arn "arn:aws:sns:us-east-1:882985546393:mp2" --message "Congratulations, you sucessfully subscribed"
 
 #SEND SMS WHEN CLOUD WATCH METRIC TRIGGERED
 aws cloudwatch put-metric-alarm --alarm-name cpumon30 --alarm-description "Alarm when CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold  --dimensions "Name=AutoScalingGroupName,Value=itmo-544-extended-auto-scaling-group-2" --evaluation-periods 1 --alarm-actions $ARN --unit Percent
